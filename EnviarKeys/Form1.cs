e2001abc236aa2e379bd49a10cafe41b5feee515
Ulises Avila Camargo <ulisesavila04@gmail.com>
2017-11-14 00:15:28 -0600

    Full project added to git repo

---
+++ EnviarKeys/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft;
+
+namespace EnviarKeys
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("User32.dll")]
+        static extern int SetForegroundWindow(IntPtr point);
+
+        [Flags]
+        private enum InputType
+        {
+            Mouse = 0,
+            Keyboard = 1,
+            Hardware = 2
+        }
+
+        [Flags]
+        private enum KeyEventF
+        {
+            KeyDown = 0x0000,
+            ExtendedKey = 0x0001,
+            KeyUp = 0x0002,
+            Unicode = 0x0004,
+            Scancode = 0x0008,
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern uint SendInput(uint nInputs, Input[] pInputs, int cbSize);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetMessageExtraInfo();
+
+        public static void SendKey(ushort key)
+        {
+            Input[] inputs =
+            {
+        new Input
+        {
+            type = (int) InputType.Keyboard,
+            u = new InputUnion
+            {
+                ki = new KeyboardInput
+                {
+                    wVk = 0,
+                    wScan = key,
+                    dwFlags = (uint) (KeyEventF.KeyDown | KeyEventF.Scancode),
+                    dwExtraInfo = GetMessageExtraInfo()
+                }
+            }
+        }
+    };
+
+            SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(Input)));
+        }
+
+        private struct Input
+        {
+            public int type;
+            public InputUnion u;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        private struct InputUnion
+        {
+            [FieldOffset(0)]
+            public readonly MouseInput mi;
+            [FieldOffset(0)]
+            public KeyboardInput ki;
+            [FieldOffset(0)]
+            public readonly HardwareInput hi;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct MouseInput
+        {
+            public readonly int dx;
+            public readonly int dy;
+            public readonly uint mouseData;
+            public readonly uint dwFlags;
+            public readonly uint time;
+            public readonly IntPtr dwExtraInfo;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct KeyboardInput
+        {
+            public ushort wVk;
+            public ushort wScan;
+            public uint dwFlags;
+            public readonly uint time;
+            public IntPtr dwExtraInfo;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct HardwareInput
+        {
+            public readonly uint uMsg;
+            public readonly ushort wParamL;
+            public readonly ushort wParamH;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Process proc = Process.GetProcessesByName("MapleStory")[0];
+            IntPtr ptrFF = proc.MainWindowHandle;
+            SetForegroundWindow(ptrFF);
+
+            SendKey(0x11);
+        }
+    }
+}
